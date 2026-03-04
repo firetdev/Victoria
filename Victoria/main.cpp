0e9709e33478b0eeed7391fa8304e87bcaead806
firetdev <futuregamesstore@gmail.com>
2026-03-03 20:26:39 -0600

    Initial Commit

---
+++ Victoria/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Victoria
+//
+//  Created by Yeshaya Zeff on 3/3/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
